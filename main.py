d388f8cd6706a908dd9a1d0ec2a3ec6bf4a4f867
Marco Brenes <dev.virco@gmail.com>
2016-12-10 19:48:40 +0000

    house cleaning

--- main.py
+++ main.py
@@ -58,24 +58,27 @@ def valid_year(year):
 
 # Main page class
 class MainPage(webapp2.RequestHandler):
-	def get(self):
+    # REST GET
+    def get(self):
 		self.write_form()
-
-	def post(self):
-		user_month = self.request.get('month')
-		user_day = self.request.get('day')
-        	user_year = self.request.get('year')
-
-        	month = valid_month(user_month)
-		day = valid_day(user_day)
-       		year = valid_year(user_year)
-
-        	if not (month and day and year):
-			self.write_form("That doesn't look valid to me, friend.",user_month, user_day, user_year)
-		else:
-			self.response.out.write("Thanks! That's a totally valid day!")
-
-	def write_form(self, error="", month="", day="", year=""):
-        	self.response.out.write(form % {"error": error, "month": month, "day": day, "year": year})
+    
+    # REST POST
+    def post(self):
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
+        
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
+        
+        if not (month and day and year):
+            self.write_form("That doesn't look valid to me, friend.", user_month, user_day, user_year)
+        else:
+            self.response.out.write("Thanks! That's a totally valid day!")
+    
+    # writes the HTML form
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error, "month": month, "day": day, "year": year})
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
