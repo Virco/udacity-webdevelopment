0944aa99ff4d3c2e0060672d5fd76da4954f962a
Marco Brenes <dev.virco@gmail.com>
2017-02-14 15:23:59 +0000

    added API capabilities to ASCIIChan

--- ASCIIChan/asciichan.py
+++ ASCIIChan/asciichan.py
@@ -29,28 +29,42 @@ class Handler(webapp2.RequestHandler):
 def art_key(name = 'default'):
     return db.Key.from_path('arts', name)
 
-IP_URL = "http://api.hostip.info/?ip="
-#http://freegeoip.net/xml/68.71.173.254
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+def gmaps_img(points):
+    markers = '&'.join('markers=%s,%s' % (p.lat, p.lon) for p in points)
+    return GMAPS_URL + markers
+    
+IP_URL = "http://freegeoip.net/xml/"
 def get_coords(ip):
+    #ip = "4.2.2.2"
+    #ip = "23.24.209.141"
     url = IP_URL + ip
     content = None
-    
     try:
         content = urllib2.urlopen(url).read()
     except URLError:
         return
     
     if content:
+        # d = minidom.parseString(content)
+        # coords = d.getElementsByTagName("gml:coordinates")
+        # if coords and coords[0].childNode[0].nodeValue:
+        #     lan, lat = coords[0].childNode[0].nodeValue.split(',')
+        #     return db.GeoPt(lat, lan)
         d = minidom.parseString(content)
-        coords = d.getElementsByTagName("gml:coordinates")
-        if coords and coords[0].childNode[0].nodeValue:
-            lan, lat = coords[0].childNode[0].nodeValue.split(',')
+        coordsLong = d.getElementsByTagName("Longitude")
+        coordsLat = d.getElementsByTagName("Latitude")
+        if coordsLong and coordsLat and coordsLong[0] and coordsLat[0]:
+            lan = coordsLong[0].childNodes[0].nodeValue
+            lat = coordsLat[0].childNodes[0].nodeValue
             return db.GeoPt(lat, lan)
+        
 
 class Art(db.Model):
     title = db.StringProperty(required = True)
     art = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
+    coords = db.GeoPtProperty()
 
 class MainPage(Handler):
     def render_front(self, error = '', title = '', art = ''):
@@ -59,10 +73,27 @@ class MainPage(Handler):
                             "WHERE ANCESTOR IS :1 "
                             "ORDER BY created DESC "
                             "LIMIT 10", art_key())
-        self.render('front.html', title = title, art = art, error = error, arts = arts)
+         
+        #prevent the running of multiple queries                   
+        arts = list(arts)
+                
+        #find which arts have coords
+        points = filter(None, (a.coords for a in arts))
+        
+        #if we have any coords, make an image url
+        img_url = None
+        if points:
+            img_url = gmaps_img(points)
+            
+        #display the image url
+        
+        self.render('front.html', title = title, art = art, error = error, 
+                                    arts = arts, img_url = img_url)
+        
         
     def get(self):
-        self.write(repr(get_coords(self.request.remote_addr)))
+        # self.write(self.request.remote_addr)
+        # self.write(repr(get_coords(self.request.remote_addr)))
         return self.render_front()
         
     def post(self):
@@ -71,11 +102,15 @@ class MainPage(Handler):
         
         if title and art:
             p = Art(parent = art_key(), title = title, art = art)
-            p.put()
+            
             #look up the user's coordinates from their IP
+            coords = get_coords(self.request.remote_addr)
+            
             #if we have coordinates, add them to the art
+            if coords:
+                p.coords = coords
             
-            
+            p.put()
             self.redirect('/')
         else:
             error = "we need both a title and some artwork!"
