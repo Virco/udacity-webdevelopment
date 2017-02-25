9f763cd82b1095a7109fa578afa9d74c8773b908
Marco Brenes <dev.virco@gmail.com>
2017-02-25 05:19:41 +0000

    restructured wiki code files, main page is loading.

---
+++ wiki/main.py
@@ -0,0 +1,21 @@
+import os
+import utils
+import webapp2
+from google.appengine.api import memcache
+from wiki import WikiPage
+
+PAGE_RE = '^[/]?[\S]+?$'
+
+DEBUG = bool(os.environ['SERVER_SOFTWARE'].startswith('Development'))
+app = webapp2.WSGIApplication([(PAGE_RE, WikiPage)], debug=DEBUG)
+                                
+# app = webapp2.WSGIApplication([('/signup', Signup), 
+#                                 ('/login', Login), 
+#                                 ('/logout', Logout), 
+#                                 ('/_edit' + PAGE_RE, EditPage), 
+#                                 (PAGE_RE, WikiPage), ], 
+#                                 debug=DEBUG)
+
+
+
+        
